feed6999e84ed82c961a08a68ef691032322fc8d
Ishan-byte67 <jaydeeph@gmail.com>
2026-06-22 20:49:08 +0100

    Add files via upload

---
+++ andor.py
@@ -0,0 +1,22 @@
+a = 10 
+b = -10
+c = 0
+
+if a and b and c :
+    print("All are boolean")
+else:
+    print("At least 1 or 2 are boolean")
+
+d = 10
+e = -10
+f = 0 
+
+if d > 0 or e > 0:
+    print("Both are greater than 0")
+else:
+    print("Both are less than 0")
+
+if e > 0 or f > 0:
+    print("True")
+else:
+    print("False")
---
+++ not.py
@@ -0,0 +1,21 @@
+a = 10 
+b = 67
+c = 67
+
+print(not(a == b))
+print(not(b == c))
+
+x = "python"
+y = "coding"
+
+if (not(x == y)):
+    print("Both are different")
+
+d = 5 
+e = 8
+
+if not((d == 1)==(e == 8)):
+    print("Hello")
+f = int(input("enter"))
+if not(f % 2 == 0):
+    print("f is odd")
